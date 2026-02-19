531054abede2129c9a915e940998862d06ec9fde
Owen <77976026+Owen7000@users.noreply.github.com>
2026-02-19 16:54:17 +0000

    Implement CORS, and basic middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const loginRoutes = require("./routes/login.routes");
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Force the use of Access-Control-Allow-Origin: *
+
+app.use('/', (req, res, next) => {
+    console.log(`Request type: ${req.method}`);
+    next();
+})
 
 app.get("/health", (_req, res) => {
     res.json({ ok: true});
 });
 
 app.use("/api/", loginRoutes);
+
+// Do not move this, keep it at the bottom of the middleware stack, otherwise it won't pick up the 404 errors properly
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!");
+})
 
 module.exports = app;
